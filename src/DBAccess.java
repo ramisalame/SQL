fe101d8f8f0cd4c3ad6ea5f8b5c59cc0d1279983
ramisalame <ramisalame@hotmail.com>
2023-04-06 22:57:48 -0400

    Update DBAccess.java

--- src/DBAccess.java
+++ src/DBAccess.java
@@ -1,19 +1,40 @@
-
 import java.sql.*;
 
 public class DBAccess {
 
-
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/";
+        String url = "jdbc:postgresql://localhost:5432/postgres"; // Update with your actual database name
         String user = "postgres";
         String password = "";
+
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
+
+
+            String sql = "INSERT INTO test (column1, column2, column3) VALUES (?, ?, ?)"; // Update with your actual table name and columns
+
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            // Set values for the parameters in the SQL statement
+            pstmt.setString(1, "yes"); // Update with the actual values to be inserted
+            pstmt.setString(2, "sio");
+            pstmt.setString(3, "valusse3");
+
+
+            int rowsAffected = pstmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Data added to the database successfully.");
+            } else {
+                System.out.println("Failed to add data to the database.");
+            }
+
+            pstmt.close();
             conn.close();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
